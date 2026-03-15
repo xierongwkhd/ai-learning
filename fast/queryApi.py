fbc97de28b159b2816828951fc8f4f31f80ce301
moke <a1510543036@gmail.com>
2026-03-15 12:29:50 +0800

    fastapi

--- fast/queryApi.py
+++ fast/queryApi.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from pathlib import Path
+
 from fastapi import APIRouter
 
 query = APIRouter(prefix='/query', tags=['查询模块'])
@@ -6,3 +9,19 @@ query = APIRouter(prefix='/query', tags=['查询模块'])
 async def query_all():
     print('查询')
     return {'msg': '列表'}
+
+
+@query.get('/queryById/{id}', summary='查询单笔数据')
+async def query_all(id: int):
+    print(f'查询{id}')
+    return {'msg': id}
+
+class UPDATE_NAME(Enum):
+    user1 = '用户1'
+    user2 = '用户2'
+
+@query.put('/updateByName/{name}', summary='更新单笔数据')
+async def updateByName(name: UPDATE_NAME):
+    print(f'更新name{name.name}')
+    print(f'更新value{name.value}')
+    return {'msg': name}
